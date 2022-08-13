5b5176d355d3ab40b761a5cc14d4e37a659235dc
shotaro <shotaro326@gmail.com>
2022-08-13 15:32:36 +0900

    fixed meta

--- pages/components/meta.js
+++ pages/components/meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Meta(props) {
   const titleJa = "株式会社Ketchup";
   const titleEn = "Ketchup Inc.";
-  const title = props.title ? `${props.title} | ${titleEn}` : titleEn;
+  const title = props.title !== '' ? `${props.title} | ${titleEn}` : titleEn;
   const description = titleEn;
 
   return (
