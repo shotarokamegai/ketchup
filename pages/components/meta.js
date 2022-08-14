7fdb7105d73d1863b8f8232196101d09bfeff594
shotaro <shotaro326@gmail.com>
2022-08-14 09:32:08 +0900

    fixed meta

--- pages/components/meta.js
+++ pages/components/meta.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import { useRouter } from "next/router";
 
 export default function Meta(props) {
+  const router = useRouter();
+  const url = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
   const titleJa = "株式会社Ketchup";
   const titleEn = "Ketchup Inc.";
   const title = props.title !== '' ? `${props.title} | ${titleEn}` : titleEn;
@@ -9,6 +12,8 @@ export default function Meta(props) {
   return (
     <Head>
       <title>{title}</title>
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
       <meta property="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
