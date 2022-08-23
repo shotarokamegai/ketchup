ba019d6657169f1cc3f2271783f291d3f92dc52e
shotaro <shotaro326@gmail.com>
2022-08-23 19:43:31 +0900

    so far

--- pages/index.js
+++ pages/index.js
@@ -114,6 +114,7 @@ function Home(props) {
                   </div>
                   <div>
                       <h2 className="slogan roc">
+                        
                           {/* We create design <br/>+ development<br/> to keep balance<br/> around ketchup.  */}
                           We know the internet and people in equal measure. 
                       </h2>
