d9b71d5628527ee319cca734086b88dbdc5a7b3e
shotaro <shotaro326@gmail.com>
2022-09-11 22:17:35 +0900

    initial commit

--- components/fetcher.js
+++
@@ -1,5 +0,0 @@
-export default function fetcher(...urls) {
-  const f = url => fetch(url).then(r => r.json())
-  return Promise.all(urls.map(f))
-}
-
--- pages/404.js
+++
@@ -1,7 +0,0 @@
-import DefaultErrorPage from 'next/error'
-
-export default function Error() {
-  return (
-    <DefaultErrorPage statusCode={404} />
-  )
-}
