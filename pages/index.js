3f1738cae04010a8f0906d6c558acb0c65b76e8d
shotaro <shotaro326@gmail.com>
2023-06-24 12:27:14 +0900

    changed meta

--- pages/index.js
+++ pages/index.js
@@ -159,12 +159,12 @@ function Home(props) {
   return(
     <>
     <Head>
-      <title>Ketchup Inc. | 株式会社 Kｅｔｃｈｕｐ</title>
+      <title>株式会社 Kｅｔｃｈｕｐ | Ketchup Inc.</title>
       <meta property="og:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}`} />
       <meta property="og:type" content="website" />
-      <meta property="description" content="Add a bit of flavoring to those contents, and deliver them in an even better thing. | デザインから幅広いWebサイト制作まで、一貫した企画制作・運用。" />
-      <meta property="og:title" content="Ketchup Inc. | 株式会社 Kｅｔｃｈｕｐ" />
-      <meta property="og:description" content="Add a bit of flavoring to those contents, and deliver them in an even better thing. | デザインから幅広いWebサイト制作まで、一貫した企画制作・運用。" />
+      <meta property="description" content="デザインから幅広いWebサイト制作まで、一貫した企画制作・運用。 | Add a bit of flavoring to those contents, and deliver them in an even better thing." />
+      <meta property="og:title" content="株式会社 Kｅｔｃｈｕｐ | Ketchup Inc." />
+      <meta property="og:description" content="デザインから幅広いWebサイト制作まで、一貫した企画制作・運用。 | Add a bit of flavoring to those contents, and deliver them in an even better thing." />
       <meta property="og:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/ogp.jpg`} />
       <meta name="twitter:card" content="summary_large_image"/>
       <link rel="icon" href={`${process.env.NEXT_PUBLIC_SITE_URL}/favicon.ico`} />
