b2b215c6c90673b5f541491724723c68e088dc65
かめがい　しょうたろう <shotaro@kamegaishoutarounoMacBook-Pro.local>
2024-06-20 18:48:26 +0900

    so far

--- pages/index.js
+++ pages/index.js
@@ -101,81 +101,143 @@ function Home(props) {
     })
   }
   const setAnimation = () => {
-    let animation1 = gsap.timeline();
-    let slogan1 = gsap.timeline();
-    let slogan2 = gsap.timeline();
-    let slogan3 = gsap.timeline();
-    let slogan4 = gsap.timeline();
-    let sloganScrub = .5;
-    let opacity = .5;
-    let y = -100;
-
-    if (window.innerWidth < 750) {
-      opacity = opacity*.5
-      y = y*.5
-    }
-
-    ScrollTrigger.create({
-        animation: animation1,
-        trigger: ".container",
-        start: "0",
-        end: "8%",
+    const parallax = document.getElementsByClassName('parallax');
+    gsap.to('#top-logo', {
+      scale: () => `1.5`,
+      alpha: () => `0`,
+      // ease: "power1.out",
+      scrollTrigger: {
+        trigger: '.container',
+        start: `0`, 
+        end: `5%`,
         scrub: .5,
-        invalidateOnRefresh: true,
-        // markers: true
+        invalidateOnRefresh: true
+      }
     });
-    ScrollTrigger.create({
-        animation: slogan1,
-        trigger: ".container",
-        start: "0",
-        end: "5%",
-        scrub: sloganScrub,
-        invalidateOnRefresh: true,
-        // markers: true
-    });
-    ScrollTrigger.create({
-        animation: slogan2,
-        trigger: ".container",
-        start: "1%",
-        end: "6%",
-        scrub: sloganScrub - .1,
-        invalidateOnRefresh: true,
-        // markers: true
-    });
-    ScrollTrigger.create({
-        animation: slogan3,
-        trigger: ".container",
-        start: "2%",
-        end: "7%",
-        scrub: sloganScrub - .2,
-        invalidateOnRefresh: true,
-        // markers: true
-    });
-    ScrollTrigger.create({
-        animation: slogan4,
-        trigger: ".container",
-        start: "3%",
-        end: "8%",
-        scrub: sloganScrub - .3,
-        invalidateOnRefresh: true,
-        // markers: true
-    });
-    animation1.to("#top-logo", {
-      y: y,opacity: opacity
-    },0)
-    slogan1.to(".slogan1", {
-      y: y,opacity: opacity
-    },0)
-    slogan2.to(".slogan2", {
-      y: y,opacity: opacity
-    },0)
-    slogan3.to(".slogan3", {
-      y: y,opacity: opacity
-    },0)
-    slogan4.to(".slogan4", {
-      y: y,opacity: opacity
-    },0)
+    for (let i = 0; i < parallax.length; i++) {
+      const elm = parallax[i];
+      const start = elm.getAttribute('data-start');
+      const end = elm.getAttribute('data-end');
+      gsap.to(elm, {
+        y: () => `${-window.innerWidth*.1}`,
+        // ease: "power1.out",
+        scrollTrigger: {
+          trigger: '.container',
+          start: `${start}`, 
+          end: `${end}`,
+          scrub: .5,
+          invalidateOnRefresh: true
+        }
+      });
+    }
+    // ScrollTrigger.create({
+    //   pin: true, //トリガー要素を固定する
+    //   trigger: ".container",
+    //   startTrigger: ".slogan",
+    //   start: "top top",
+    //   endTrigger: ".slogan",
+    //   end:`top top-=${window.innerHeight}`,
+    //  // サイドメニューの高さ125px+start位置の50px=1７５px
+    //   markers: true,
+    // });
+    // gsap.to('.slogan', {
+    //   y: () => `${window.innerWidth*.486111111111}`,
+    //   // ease: "power1.out",
+    //   scrollTrigger: {
+    //     pin: true,
+    //     markers: true,
+    //     trigger: '.container',
+    //     start: `top top`, 
+    //     end: `top top+=${window.innerHeight}`,
+    //     scrub: .5,
+    //     invalidateOnRefresh: true
+    //   }
+    // });
+    // ScrollTrigger.create({
+    //     trigger: ".container",
+    //     start: "0",
+    //     end: `${window.innerHeight}`,
+    //     scrub: .5,
+    //     invalidateOnRefresh: true,
+    //     // markers: true
+    // });
   }
+  // const setAnimation = () => {
+  //   let animation1 = gsap.timeline();
+  //   let slogan1 = gsap.timeline();
+  //   let slogan2 = gsap.timeline();
+  //   let slogan3 = gsap.timeline();
+  //   let slogan4 = gsap.timeline();
+  //   let sloganScrub = .5;
+  //   let opacity = .5;
+  //   let y = -100;
+
+  //   if (window.innerWidth < 750) {
+  //     opacity = opacity*.5
+  //     y = y*.5
+  //   }
+
+  //   ScrollTrigger.create({
+  //       animation: animation1,
+  //       trigger: ".container",
+  //       start: "0",
+  //       end: "8%",
+  //       scrub: .5,
+  //       invalidateOnRefresh: true,
+  //       // markers: true
+  //   });
+  //   ScrollTrigger.create({
+  //       animation: slogan1,
+  //       trigger: ".container",
+  //       start: "0",
+  //       end: "5%",
+  //       scrub: sloganScrub,
+  //       invalidateOnRefresh: true,
+  //       // markers: true
+  //   });
+  //   ScrollTrigger.create({
+  //       animation: slogan2,
+  //       trigger: ".container",
+  //       start: "1%",
+  //       end: "6%",
+  //       scrub: sloganScrub - .1,
+  //       invalidateOnRefresh: true,
+  //       // markers: true
+  //   });
+  //   ScrollTrigger.create({
+  //       animation: slogan3,
+  //       trigger: ".container",
+  //       start: "2%",
+  //       end: "7%",
+  //       scrub: sloganScrub - .2,
+  //       invalidateOnRefresh: true,
+  //       // markers: true
+  //   });
+  //   ScrollTrigger.create({
+  //       animation: slogan4,
+  //       trigger: ".container",
+  //       start: "3%",
+  //       end: "8%",
+  //       scrub: sloganScrub - .3,
+  //       invalidateOnRefresh: true,
+  //       // markers: true
+  //   });
+  //   animation1.to("#top-logo", {
+  //     y: y,opacity: opacity
+  //   },0)
+  //   slogan1.to(".slogan1", {
+  //     y: y,opacity: opacity
+  //   },0)
+  //   slogan2.to(".slogan2", {
+  //     y: y,opacity: opacity
+  //   },0)
+  //   slogan3.to(".slogan3", {
+  //     y: y,opacity: opacity
+  //   },0)
+  //   slogan4.to(".slogan4", {
+  //     y: y,opacity: opacity
+  //   },0)
+  // }
   let i = 0
 
   console.log('here')
@@ -195,7 +257,7 @@ function Home(props) {
     //TOP用アニメーション
     const loadFunc = () => {
       ScrollTrigger.refresh()
-      // setAnimation();
+      setAnimation();
       showList();
     }
 
@@ -235,14 +297,15 @@ function Home(props) {
         <main id="home" className="main_">
           <section id="top">
               <div className="ruler">
-                  <h1 id="top-logo" className="logo" ref={logoRef}>
+                  <h1 id="top-logo" className="logo fixed fade" data-start="0" data-end="10%" ref={logoRef}>
+                  {/* <h1 id="top-logo" className="logo absolute parallax" data-start="0" data-end="10%" ref={logoRef}> */}
                     <Logo />
                   </h1>
-                  <p className="est red flex flex-end">
+                  <div className="est red flex flex-end absolute parallax" data-start="0%" data-end="15%">
                     <span className="roc-grotesk">EST.</span>
                     <span className="borax italic">2022</span>
-                  </p>
-                  <div className="mix-text red vertical absolute scroll-trigger" data-target="works">
+                  </div>
+                  <div className="mix-text red vertical absolute scroll-trigger parallax" data-start="0%" data-end="25%" data-target="works">
                     <div className="mix-text__inner">
                       <span className="roc-grotesk-wide">SCROLL</span>
                       <span className="borax italic">Down</span>
@@ -253,11 +316,9 @@ function Home(props) {
                       </span>
                     </div>
                   </div>
-                  <div>
-                      <h2 className="slogan borax red">
-                      Add a bit of flavoring to those contents,<br/>and deliver them in an even better thing.
-                      </h2>
-                  </div>
+                  <h2 className="slogan borax red">
+                  Add a bit of flavoring to those contents,<br/>and deliver them in an even better thing.
+                  </h2>
               </div>
           </section>
           <section id="works" className="section">
